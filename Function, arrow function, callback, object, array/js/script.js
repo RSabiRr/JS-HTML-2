5affb500d264a5a74dd05e51dfa31aa7774d9d72
RSabiRr <92287092+RSabiRr@users.noreply.github.com>
2021-12-05 23:53:03 +0400

    Add files via upload

---
+++ Function, arrow function, callback, object, array/js/script.js
@@ -0,0 +1,170 @@
+
+// task 1
+
+// let arr=[];
+// arr.push(1);
+// arr.push(3);
+// arr.push(5);
+
+// let newArr=[2,6,8];
+
+// arr=arr.concat(newArr);
+
+// console.log(arr);
+
+////////////////////////////////
+//task 2 
+
+// let arr=[1,2,3,4,5];
+
+// console.log(arr.includes(3));
+
+// let arr2=[1,1,2,1,1];
+
+// console.log(arr2.includes(3));
+
+// let arr3=[5,5,5,6];
+
+// console.log(arr3.includes(5));
+
+// let arr4=[];
+
+// console.log(arr2.includes(5));
+
+//////////////////////////////
+
+//task3
+
+// let arr=[2,3,4];
+// let arr=[1,2,3,8];
+
+
+
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr.length%12==0) {
+//             console.log(true);
+//         }else{
+//             console.log(false);
+//         }
+//     }
+
+////////////////////////////////////////////
+
+//task 4
+
+// let arr=[];
+
+// for (let i = 1; i <= 5; i++) {
+//     arr.push(i);
+// }
+
+// console.log(arr);
+
+// let arr1=[];
+
+// for (let i = 98; i <= 100; i++) {
+//     arr1.push(i);
+// }
+
+// console.log(arr1);
+
+//////////////////////////////////////
+
+//task 5
+
+
+// let arr=[32,25,85,89,45,76,7,41,2,12];
+
+//a
+// console.log(arr);
+
+//b
+// console.log(Math.max.apply(null,arr));
+
+//c
+// arr.push(15);
+// console.log(arr);
+
+//d
+// console.log(arr);
+// console.log(arr.pop());
+// console.log(arr);
+
+//e
+// console.log(arr.sort((a,b)=>a-b));
+// console.log(arr[arr.length-1]);
+
+//f
+// console.log(arr);
+// arr.splice(3,0,43);
+// console.log(arr);
+
+//g
+// console.log(arr);
+// console.log(arr.slice(3));
+
+//h
+// arr.forEach(element => {
+//     console.log(element);
+// });
+
+//i
+// for (let i = 0; i < arr.length; i++) {
+    
+//     if (arr[i]>40) {
+//         console.log(arr[i]);
+//     }
+
+// }
+
+///////////////////////////////////
+
+// function month(number) {
+    
+//     switch(number){
+//         case 1:
+//             return "Yanvar";
+//             break;
+//         case 2:
+//                 return "Fevral";
+//                 break;   
+//         case 3:
+//                  return "Mart";
+//                   break; 
+//         case 4:
+//                  return "Aprel";
+//                  break;
+//         case 5:
+//                   return "May";
+//                  break;
+//         case 6:
+//                   return "Iyun";
+//                  break;
+//         case 7:
+//                   return "Iyul";
+//                  break;
+//         case 8:
+//                   return "Avqust";
+//                  break;
+//         case 9:
+//                   return "Sentyabr";
+//                  break;
+//         case 10:
+//                   return "Oktyabr";
+//                  break;
+//         case 11:
+//                   return "Noyabr";
+//                  break;
+//         case 12:
+//                   return "Dekabr";
+//                  break;
+//     }
+// }
+
+// let input=prompt("Fesli secin: ")
+
+// console.log(month(parseInt(input)));
+
+//////////////////////
+
+ 
